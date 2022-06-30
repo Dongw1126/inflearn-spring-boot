b99265486923ffd0b5f8dd1119f87dbf9499eb35
Dongw1126 <topblade71@naver.com>
2022-06-30 14:23:35 +0900

    feat: 정적콘텐츠, MVC, API 기능

--- payz-intern/src/main/java/bccard/payzintern/controller/HelloController.java
+++ payz-intern/src/main/java/bccard/payzintern/controller/HelloController.java
@@ -3,6 +3,8 @@ package bccard.payzintern.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 // Controller 로 쓰려면 어노테이션 붙여줘야함
 @Controller
@@ -16,4 +18,41 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         return "hello";
     }
+
+    @GetMapping("hello-mvc")
+    // @RequestParam 을 이용해서 url 파라미터 받아옴
+    // @RequestParam("name") 만 넣고 hello-mvc로 들어가면 에러 -> 파라미터가 없음
+    public String helloMvc(@RequestParam(value="name") String name, Model model) {
+        model.addAttribute("name", name);
+
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    // @ResponseBody -> http 바디에 return의 내용을 넣어주겠다는 의미
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
